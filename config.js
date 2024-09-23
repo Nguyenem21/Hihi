3b37c3d47efbfd0ecef413071772af94a416d599
Nguyenem21 <111334079+Nguyenem21@users.noreply.github.com>
2024-09-23 20:02:13 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,17 @@
+const CONFIG = {
+    titleWeb: "Đọc Là Nguu",
+    introTitle: 'Gửi Bạn Huyền',
+    introDesc: `Thiên Lý Ơi`,
+    btnIntro: '^^HiHi^^',
+    title: 'Huyền Có Nguu Không',
+    desc: '??? ',
+    btnYes: 'Cóoo',
+    btnNo: 'Không nhaa',
+    question: ':33',
+    btnReply: '<3',
+    reply: 'huyền tre trau',
+    mess: ':333',
+    messDesc: '<3',
+    btnAccept: '<3',
+    messLink: 'https://www.facebook.com/lousie.kamui' 
+}
